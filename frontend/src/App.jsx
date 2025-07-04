dbdfbfb478602d66e8fc649be648096149291acf
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-07-05 04:26:50 +0500

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Axios from 'axios';
 import Card from './components/card';
 
 function App() {
-  //const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001'; -> For Local
-  const baseUrl = ''; //When deploying to AWS ECS
+  const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001'; -> For Local
+  //const baseUrl = ''; //When deploying to AWS ECS
 
   const [values, setValues] = useState({});
   const [games, setGames] = useState([]);
